439e6867d8d5458953f735b09c38b02140c8070a
beafromsea <beatrizdoots@gmail.com>
2023-08-15 16:38:18 -0300

    Atualização do schema

--- scripts/projeto-grupo.sql
+++ scripts/projeto-grupo.sql
@@ -1,72 +1,121 @@
 CREATE DATABASE bd_resilia;
 
 USE bd_resilia;
-    
+
+CREATE TABLE `Enderecos` (
+  `endereco_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `rua` VARCHAR(100) NOT NULL,
+  `numero` VARCHAR(7) NOT NULL,
+  `complemento` VARCHAR(50),
+  `bairro` VARCHAR(25) NOT NULL,
+  `cidade` VARCHAR(50) NOT NULL,
+  `uf` VARCHAR(2) NOT NULL,
+  `cep` VARCHAR(8) NOT NULL
+);
+
 CREATE TABLE `Pessoas` (
-  `id` INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(60),
-  `data_nasc` DATE,
-  `cpf` BIGINT,
-  `rg` VARCHAR(10),
-  `endereco` VARCHAR(100),
-  `telefone` BIGINT,
-  `email` VARCHAR(60),
-  `pcd` VARCHAR(3)
-);
-
-CREATE TABLE `Desempenhos` (
-  `id` INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `nota_indiv` INT,
-  `nota_grupo` INT,
-  `frequencia` VARCHAR(60)
-);
-
-CREATE TABLE `Modulos` (
-  `id` INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(100),
-  `descricao` VARCHAR(100),
-  `qtd_aulas` INT,
-  `carga_horaria` INT,
-  `intensidade` INT
+  `pessoa_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `nome` VARCHAR(60) NOT NULL,
+  `data_nasc` DATE NOT NULL,
+  `cpf` VARCHAR(11) NOT NULL,
+  `telefone` VARCHAR(15) NOT NULL,
+  `email` VARCHAR(60) NOT NULL,
+  `nivel_escolaridade` VARCHAR(60) NOT NULL,
+  `pcd` VARCHAR(5),
+  `endereco_id` INT NOT NULL
 );
 
 CREATE TABLE `Estudantes` (
-  `id` INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `nivel_escolaridade` VARCHAR(60),
-  `nivel_aprov` VARCHAR(100),
-  `id_desempenho_fk` INT,
-  `id_pessoas_fk` INT,
-   FOREIGN KEY (`id_desempenho_fk`) REFERENCES `Desempenhos` (`id`),
-   FOREIGN KEY (`id_pessoas_fk`) REFERENCES `Pessoas` (`id`)
+  `estudante_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `senha` VARCHAR(32) NOT NULL,
+  `pessoa_id` INT NOT NULL
 );
 
 CREATE TABLE `Facilitadores` (
-  `id` INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `formacao` VARCHAR(100),
-  `especialidade` VARCHAR(100),
-  `id_pessoas_fk` INT,
-  FOREIGN KEY (`id_pessoas_fk`) REFERENCES `Pessoas` (`id`)
+  `facilitador_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `formacao` VARCHAR(50) NOT NULL,
+  `frente` ENUM ('hard', 'soft', 'monitoria') NOT NULL,
+  `pessoa_id` INT NOT NULL
 );
 
 CREATE TABLE `Cursos` (
-  `id` INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `duracao` INT,
-  `id_modulos_fk` INT,
-  FOREIGN KEY (`id_modulos_fk`) REFERENCES `Modulos` (`id`)
+  `curso_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `nome` VARCHAR(50) NOT NULL,
+  `duracao` INT NOT NULL
+);
+
+CREATE TABLE `Modulos` (
+  `modulo_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `nome` VARCHAR(50) NOT NULL,
+  `qtd_aulas` INT NOT NULL,
+  `carga_horaria` INT NOT NULL
 );
 
 CREATE TABLE `Turmas` (
-  `id` INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `organizacao` VARCHAR(100),
-  `nome` VARCHAR(100),
-  `qtd_alunos` INT,
-  `facilitador` INT,
-  `qtd_modulos` INT,
-  `turno` ENUM('manha', 'tarde', 'noite')
+  `turma_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `nome` VARCHAR(50) NOT NULL,
+  `organizacao` VARCHAR(50),
+  `data_inicio` DATE,
+  `turno` ENUM ('manha', 'tarde', 'noite') NOT NULL,
+  `curso_id` INT NOT NULL
 );
 
-ALTER TABLE `Estudantes` ADD FOREIGN KEY (`id_desempenho_fk`) REFERENCES `Desempenhos` (`id`);
+CREATE TABLE `Desempenhos` (
+  `desempenho_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `nota_indiv` INT NOT NULL,
+  `nota_grupo` INT NOT NULL,
+  `frequencia` INT NOT NULL
+);
 
-ALTER TABLE `Estudantes` ADD FOREIGN KEY (`id_pessoas_fk`) REFERENCES `Pessoas` (`id`);
+CREATE TABLE `Desempenhos_estudante` (
+  `desempenho_estudante_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `modulo_id` INT NOT NULL,
+  `desempenho_id` INT NOT NULL,
+  `estudante_id` INT NOT NULL
+);
 
-ALTER TABLE `Facilitadores` ADD FOREIGN KEY (`id_pessoas_fk`) REFERENCES `Pessoas` (`id`);
+CREATE TABLE `Curso_modulo` (
+  `curso_id` INT NOT NULL,
+  `modulo_id` INT NOT NULL
+);
+
+CREATE TABLE `Estudante_turma` (
+  `matricula` VARCHAR(8) NOT NULL,
+  `status` ENUM ('ativo','inativo') NOT NULL,
+  `estudante_id` INT NOT NULL,
+  `turma_id` INT NOT NULL,
+  `desempenho_estudante_id` INT NOT NULL
+);
+
+CREATE TABLE `Facilitador_turma` (
+  `facilitador_id` INT NOT NULL,
+  `turma_id` INT NOT NULL
+);
+
+ALTER TABLE `Pessoas` ADD FOREIGN KEY (`endereco_id`) REFERENCES `Enderecos` (`endereco_id`);
+
+ALTER TABLE `Estudantes` ADD FOREIGN KEY (`pessoa_id`) REFERENCES `Pessoas` (`pessoa_id`);
+
+ALTER TABLE `Facilitadores` ADD FOREIGN KEY (`pessoa_id`) REFERENCES `Pessoas` (`pessoa_id`);
+
+ALTER TABLE `Turmas` ADD FOREIGN KEY (`curso_id`) REFERENCES `Cursos` (`curso_id`);
+
+ALTER TABLE `Desempenhos_estudante` ADD FOREIGN KEY (`modulo_id`) REFERENCES `Modulos` (`modulo_id`);
+
+ALTER TABLE `Desempenhos_estudante` ADD FOREIGN KEY (`desempenho_id`) REFERENCES `Desempenhos` (`desempenho_id`);
+
+ALTER TABLE `Desempenhos_estudante` ADD FOREIGN KEY (`estudante_id`) REFERENCES `Estudantes` (`estudante_id`);
+
+ALTER TABLE `Curso_modulo` ADD FOREIGN KEY (`curso_id`) REFERENCES `Cursos` (`curso_id`);
+
+ALTER TABLE `Curso_modulo` ADD FOREIGN KEY (`modulo_id`) REFERENCES `Modulos` (`modulo_id`);
+
+ALTER TABLE `Estudante_turma` ADD FOREIGN KEY (`estudante_id`) REFERENCES `Estudantes` (`estudante_id`);
+
+ALTER TABLE `Estudante_turma` ADD FOREIGN KEY (`turma_id`) REFERENCES `Turmas` (`turma_id`);
+
+ALTER TABLE `Estudante_turma` ADD FOREIGN KEY (`desempenho_estudante_id`) REFERENCES `Desempenhos_estudante` (`desempenho_estudante_id`);
+
+ALTER TABLE `Facilitador_turma` ADD FOREIGN KEY (`facilitador_id`) REFERENCES `Facilitadores` (`facilitador_id`);
+
+ALTER TABLE `Facilitador_turma` ADD FOREIGN KEY (`turma_id`) REFERENCES `Turmas` (`turma_id`);
